cc251c0ae3c33b462087f9157a324592260cb25a
Zul Arsil Mustofa <75080485+zular-silfa@users.noreply.github.com>
2021-04-20 22:57:27 +0700

    Add files via upload

---
+++ Week 6 Sorting Data/SequentialSearch.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week6;
+
+/**
+ *
+ * @author Zul Arsil Mustofa
+ */
+public class SequentialSearch {
+
+    public static void main(String[] args) {
+        int[] exampleVariableOne = {2, 9, 6, 7, 4, 5, 3, 0, 1};
+        int target = 4;
+        sequentialSearch(exampleVariableOne, target);
+    }
+    
+    public static void sequentialSearch(int[] parameterOne, int parameterTwo) {
+        int index = -1;
+        
+        //Search each index of the array until it reachs the last index
+        for (int i = 0; i < parameterOne.length; i++) {
+            if (parameterOne[i] == parameterTwo) {
+                // if the target is found, int index is set as the value of i and
+                // the for loop is terminated
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            System.out.println("Your target integer does not exist in the array"); 
+        }else {
+            System.out.println("Your target integer is in index " + index + " of the arry");
+        }  
+    }
+}
